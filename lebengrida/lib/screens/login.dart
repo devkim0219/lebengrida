0e30d807b9e8bca4469af4b99b0fafef36c0a8d6
devkim0219 <devkim0219@gmail.com>
2020-11-12 11:31:42 +0900

    회원 정보 수정 시 기존 회원 등록 되어있는지 확인

--- lebengrida/lib/screens/login.dart
+++ lebengrida/lib/screens/login.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:lebengrida/screens/animation.dart';
 import 'package:lebengrida/screens/guide.dart';
 import 'package:lebengrida/screens/result.dart';
 import 'package:lebengrida/screens/update_info.dart';
 import 'package:lebengrida/services/login_service.dart';
 import 'package:lebengrida/services/result_service.dart';
-import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -36,33 +34,12 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   // 회원 등록 여부 체크
-  _checkUser(String mobile) {
-    if (_mobileController.text.isEmpty) {
-      Fluttertoast.showToast(
-        msg: '휴대폰 번호를 입력해주세요.',
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.BOTTOM,
-        timeInSecForIosWeb: 3
-      );
-      return;
-    }
-
-    LoginServices.checkUser(mobile).then((result) {
-      if (result == 'success') {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => GuidePage(mobile: mobile),
-          )
-        );
-      } else {
-        Fluttertoast.showToast(
-          msg: '등록된 사용자가 아닙니다. 회원등록 후 이용해주세요.',
-          toastLength: Toast.LENGTH_SHORT,
-          gravity: ToastGravity.BOTTOM,
-          timeInSecForIosWeb: 3
-        );
-      }
+  Future<String> _checkUser(String mobile) async {
+    var _result = '';
+    await LoginServices.checkUser(mobile).then((result) {
+      _result = result;
     });
+    return _result;
   }
 
   // 기존 검사 여부 체크
@@ -103,131 +80,163 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<String>.value(value: _mobileController.text),
-      ],
-      child: GestureDetector(
-        onTap: () {
-          _focusNode.unfocus();
-        },
-        child: FocusScope(
-          node: _focusNode,
-          child: WillPopScope(
-            onWillPop: () async {
-              bool result = onPressBackButton();
-              return await Future.value(result);
-            },
-            child: Scaffold(
-              key: scaffoldKey,
-              appBar: AppBar(title: Text('스마트 스피치(S-Speech)')),
-              body: SingleChildScrollView(
-                child: Container(
-                  padding: EdgeInsets.all(60.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(top: 10),
-                        child: Image(
-                          image: AssetImage('assets/images/logo.png'),
-                        ),
+    return GestureDetector(
+      onTap: () {
+        _focusNode.unfocus();
+      },
+      child: FocusScope(
+        node: _focusNode,
+        child: WillPopScope(
+          onWillPop: () async {
+            bool result = onPressBackButton();
+            return await Future.value(result);
+          },
+          child: Scaffold(
+            key: scaffoldKey,
+            appBar: AppBar(title: Text('스마트 스피치(S-Speech)')),
+            body: SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.all(60.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 10),
+                      child: Image(
+                        image: AssetImage('assets/images/logo.png'),
                       ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 50),
-                        child: Text(
-                          '스마트 화행 검사',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 30,
-                            color: Colors.teal,
-                          )
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 60),
-                        child: TextField(
-                          controller: _mobileController,
-                          decoration: InputDecoration(
-                            hintText: '휴대폰 번호를 입력하세요.'
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 30),
-                        child:Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            RaisedButton(
-                              color: Colors.teal,
-                              child: Text('회원 등록', style: TextStyle(color: Colors.white)),
-                              onPressed: () {
-                                Navigator.pushNamed(context, '/join');
-                              },
-                            ),
-                            SizedBox(
-                              width: 15,
-                            ),
-                            RaisedButton(
-                              color: Colors.teal,
-                              child: Text('로그인', style: TextStyle(color: Colors.white)),
-                              onPressed: () {
-                                _checkUser(_mobileController.text);
-                              },
-                            ),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 5),
-                        child:RaisedButton(
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 50),
+                      child: Text(
+                        '스마트 화행 검사',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
                           color: Colors.teal,
-                          child: Text('회원 정보 수정', style: TextStyle(color: Colors.white)),
-                          onPressed: () {
-                            if (_mobileController.text.isEmpty) {
+                        )
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 60),
+                      child: TextField(
+                        controller: _mobileController,
+                        decoration: InputDecoration(
+                          hintText: '휴대폰 번호를 입력하세요.'
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 30),
+                      child:Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          RaisedButton(
+                            color: Colors.teal,
+                            child: Text('회원 등록', style: TextStyle(color: Colors.white)),
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/join');
+                            },
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          RaisedButton(
+                            color: Colors.teal,
+                            child: Text('로그인', style: TextStyle(color: Colors.white)),
+                            onPressed: () {
+                              if (_mobileController.text.isEmpty) {
+                                Fluttertoast.showToast(
+                                  msg: '휴대폰 번호를 입력해주세요.',
+                                  toastLength: Toast.LENGTH_SHORT,
+                                  gravity: ToastGravity.BOTTOM,
+                                  timeInSecForIosWeb: 3
+                                );
+                                return;
+                              }
+                              _checkUser(_mobileController.text).then((value) {
+                                if (value == 'success') {
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) => GuidePage(mobile: _mobileController.text),
+                                    )
+                                  );
+                                } else {
+                                  Fluttertoast.showToast(
+                                    msg: '등록된 사용자가 아닙니다. 회원등록 후 이용해주세요.',
+                                    toastLength: Toast.LENGTH_SHORT,
+                                    gravity: ToastGravity.BOTTOM,
+                                    timeInSecForIosWeb: 3
+                                  );
+                                  return;
+                                }
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 5),
+                      child:RaisedButton(
+                        color: Colors.teal,
+                        child: Text('회원 정보 수정', style: TextStyle(color: Colors.white)),
+                        onPressed: () {
+                          if (_mobileController.text.isEmpty) {
+                            Fluttertoast.showToast(
+                              msg: '휴대폰 번호를 입력해주세요.',
+                              toastLength: Toast.LENGTH_SHORT,
+                              gravity: ToastGravity.BOTTOM,
+                              timeInSecForIosWeb: 3
+                            );
+                            return;
+                          }
+                          _checkUser(_mobileController.text).then((value) {
+                            if (value == 'success') {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => UpdateInfoPage(mobile: _mobileController.text)
+                                )
+                              );
+                            } else {
                               Fluttertoast.showToast(
-                                msg: '휴대폰 번호를 입력해주세요.',
+                                msg: '등록된 사용자가 아닙니다. 회원등록 후 이용해주세요.',
                                 toastLength: Toast.LENGTH_SHORT,
                                 gravity: ToastGravity.BOTTOM,
                                 timeInSecForIosWeb: 3
                               );
                               return;
                             }
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => UpdateInfoPage(mobile: _mobileController.text)
-                              )
-                            );
-                          },
-                        ),
+                          });
+                        },
                       ),
-                      // Padding(
-                      //   padding: EdgeInsets.only(top: 5),
-                      //   child:RaisedButton(
-                      //     color: Colors.teal,
-                      //     child: Text('회원 목록(Test)', style: TextStyle(color: Colors.white)),
-                      //     onPressed: () {
-                      //       Navigator.pushNamed(context, '/userList');
-                      //     },
-                      //   ),
-                      // ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 5),
-                        child:RaisedButton(
-                          color: Colors.teal,
-                          child: Text('최근 검사 결과', style: TextStyle(color: Colors.white)),
-                          onPressed: () {
-                            _checkTested(_mobileController.text);
-                          },
-                        ),
+                    ),
+                    // Padding(
+                    //   padding: EdgeInsets.only(top: 5),
+                    //   child:RaisedButton(
+                    //     color: Colors.teal,
+                    //     child: Text('회원 목록(Test)', style: TextStyle(color: Colors.white)),
+                    //     onPressed: () {
+                    //       Navigator.pushNamed(context, '/userList');
+                    //     },
+                    //   ),
+                    // ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 5),
+                      child:RaisedButton(
+                        color: Colors.teal,
+                        child: Text('최근 검사 결과', style: TextStyle(color: Colors.white)),
+                        onPressed: () {
+                          _checkTested(_mobileController.text);
+                        },
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            )
-          ) 
-        ),
+            ),
+          )
+        )
       ),
     );
   }
